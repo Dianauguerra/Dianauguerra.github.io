e386366da94207012f26b59baa76014c22111843
Dianauguerra <diana.1609@hotmail.com>
2022-05-13 23:56:31 -0400

    completed storm center page

--- weather/js/script.js
+++ weather/js/script.js
@@ -68,10 +68,9 @@ function adjustRating(rating) {
   document.getElementById("stormvalue").innerHTML = rating;
 }
 
-function selectResponse() {
-const s = document.querySelector('#selected')
-const sel = document.querySelector('#selectbrowser');
-s.style.display = "block";
-s.textContent = sel.value;
 
-}
+// function validateZipCode(elementValue){
+//   var zipCodePattern = /^\d{5}$/;
+//   zipCodePattern.test(elementValue);
+//   // document.getElementById("elementValue").innerHTML = elementValue;
+// }
