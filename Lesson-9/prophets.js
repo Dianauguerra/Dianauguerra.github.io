fae9bef78da7cac3f6faae57ebd5d46494347344
Dianauguerra <diana.1609@hotmail.com>
2022-05-17 16:03:19 -0400

    added and fixed some js

--- Lesson-9/prophets.js
+++ Lesson-9/prophets.js
@@ -5,18 +5,40 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
+    console.table(jsonObject);
+    const prophets = jsonObject["prophets"];
+    prophets.forEach(listprophets);
   });
-  
-  
-  for (let i = 0; i < prophets.length; i++ );
 
-    
-  let card = document.createElement('section');
-  let h2 = document.createElement('h2');
-  h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-  card.appendChild(h2);
-  document.querySelector('div.cards').appendChild(card);
+function listprophets(prophet) {
 
-  // }
+  //methods//
+  const name = prophet.name; 
+  const lastname = prophet.lastname;
+  const birthdate = prophet.birthdate;
+  const birthplace = prophet.birthplace;
+
+  //elements//
+  let card = document.createElement("section");
+  let h2 = document.createElement("h2");
+  let portrait = document.createElement("img");
+  let birthcontent = document.createElement("p");
+  let birthplaceontent = document.createElement("p");
+
+
+  //to display//
+  h2.textContent = `${name} ${lastname}`;
+  birthplaceontent.innerHTML = `<strong>Place of Birth:</strong> ${birthplace}`;
+  birthcontent.innerHTML = `<strong>Date of Birth:</strong> ${birthdate}`;
+  portrait.setAttribute("src", prophet.imageurl);
+
+
+  //appending children of cards div//
+  document.querySelector(".cards").appendChild(card);
+  document.querySelector(".cards").appendChild(h2);
+  document.querySelector(".cards").appendChild(birthcontent);
+  document.querySelector(".cards").appendChild(birthplaceontent);
+  document.querySelector(".cards").appendChild(portrait);
+
+
+}
