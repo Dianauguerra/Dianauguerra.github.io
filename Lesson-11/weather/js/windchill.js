f6bb14306d874af521604fa09968d41377ad36c9
Dianauguerra <diana.1609@hotmail.com>
2022-05-25 13:28:39 -0400

    updated windchill in lesson 11

--- Lesson-11/weather/js/windchill.js
+++ Lesson-11/weather/js/windchill.js
@@ -1,3 +1,5 @@
+////-----------------CURRENT WEATHER PRESTON----------------------////
+
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=83b661e31f3c6f04c5e62b84c6287698&units=imperial";
 
 fetch(apiURL)
@@ -25,122 +27,107 @@ fetch(apiURL)
  
 });
 //-----------------5 DAY FORECAST-------------------------//
-
-  //day 1//
-  var today = new Date();
-  var dd = String(today.getDate()+1).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); 
-  var yyyy = today.getFullYear();
   
-  const first = yyyy + "-" + mm + "-" + dd;
+// JSON 5 DAY FORECAST //
+const forecastApiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=83b661e31f3c6f04c5e62b84c6287698&units=imperial";
+fetch(forecastApiURL)
+.then((response) => response.json())
 
-  //day 2//
-  var dd = String(today.getDate()+2).padStart(2, '0');
-  const second = yyyy + "-" + mm + "-" + dd;
-
-  //day 3//
-  var dd = String(today.getDate()+3).padStart(2, '0');
-  const third = yyyy + "-" + mm + "-" + dd;
-
-  //day 4//
-  var dd = String(today.getDate()+4).padStart(2, '0');
-  const forth = yyyy + "-" + mm + "-" + dd;
-
-  //day 5//
-  var dd = String(today.getDate()+5).padStart(2, '0');
-  const fifth = yyyy + "-" + mm + "-" + dd;
-  
-  // WEEKDAYS  //
-  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-  const d = new Date();
-  const day1_content = weekday[d.getDay()];
-  const day2_content = weekday[d.getDay()+1];
-  const day3_content = weekday[d.getDay()+2];
-  const day4_content = weekday[d.getDay()+3];
-  const day5_content = weekday[d.getDay()+4];
-
-  const dayname1 = document.getElementById("dayName1");
-  dayname1.textContent = day1_content;
-
-  const dayname2 = document.getElementById("dayName2");
-  dayname2.textContent = day2_content;
-
-  const dayname3 = document.getElementById("dayName3");
-  dayname3.textContent = day3_content;
-
-  const dayname4 = document.getElementById("dayName4");
-  dayname4.textContent = day4_content;
-
-  const dayname5 = document.getElementById("dayName5");
-  dayname5.textContent = day5_content;
-
-  
-  // JSON 5 DAY FORECAST //
-  const forecastApiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=83b661e31f3c6f04c5e62b84c6287698&units=imperial";
-  fetch(forecastApiURL)
-  .then((response) => response.json())
-  
   .then((jsOnbject) => {
     let list = jsOnbject.list;
     console.log(jsOnbject);
 
-  for (i = 0; i < list.length; i++) {
-   
-    if (list[i].dt_txt == (first+ " " + "18:00:00")){
-      let temp = list[i].main.temp;
-      const icon = list[i].weather[0].icon;
-      const imgsource =  "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-      const imgalt = list[i].weather[0].description;
-      document.getElementById("1").textContent = temp;
-      document.getElementById("icon1").setAttribute("src",imgsource);
-      document.getElementById("icon1").setAttribute("alt",imgalt);
-      document.getElementById("icon1").setAttribute("width", 50);
-    };
+    var icons = []
+    var descriptions = []
+    var temps = []
+    var results = []
 
-    if (list[i].dt_txt == (second+ " " + "18:00:00")){
-      let temp = list[i].main.temp;
-      const icon = list[i].weather[0].icon;
-      const imgsource =  "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-      const imgalt = list[i].weather[0].description;
-      document.getElementById("2").textContent = temp;
-      document.getElementById("icon2").setAttribute("src",imgsource);
-      document.getElementById("icon2").setAttribute("alt",imgalt);
-    };
-    if (list[i].dt_txt == (third+ " " + "18:00:00")){
-      let temp = list[i].main.temp;
-      const icon = list[i].weather[0].icon;
-      const imgsource =  "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-      const imgalt = list[i].weather[0].description;
-      document.getElementById("3").textContent = temp;
-      document.getElementById("icon3").setAttribute("src",imgsource);
-      document.getElementById("icon3").setAttribute("alt",imgalt);
-    };
-
-    if (list[i].dt_txt == (forth+ " " + "18:00:00")){
-      let temp = list[i].main.temp;
-      const icon = list[i].weather[0].icon;
-      const imgsource =  "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-      const imgalt = list[i].weather[0].description;
-      document.getElementById("4").textContent = temp;
-      document.getElementById("icon4").setAttribute("src",imgsource);
-      document.getElementById("icon4").setAttribute("alt",imgalt);
+     for (i = 0; i < list.length; i++) {
+      var word = list[i].dt_txt;
+      
     
-    };
+      if (list[i].dt_txt.substr(11,18) == "18:00:00"){
+        // console.log(i)
+        var result = word.substr(0,10);
+        var temp = list[i].main.temp;
+        var icon = list[i].weather[0].icon;
+        var description = list[i].weather[0].description;
 
-    if (list[i].dt_txt == (fifth+ " " + "18:00:00")){
-      let temp = list[i].main.temp;
-      const icon = list[i].weather[0].icon;
-      const imgsource =  "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-      const imgalt = list[i].weather[0].description;
-      document.getElementById("5").textContent = temp;
-      document.getElementById("icon5").setAttribute("src",imgsource);
-      document.getElementById("icon5").setAttribute("alt",imgalt);
-    };
+        icons.push(icon);
+        descriptions.push(description);
+        temps.push(temp);
+        results.push(result);
 
-  }});
+      }  
 
-            
+    }
+
+  //FIRST DAY//
+  var imgsource =  "http://openweathermap.org/img/wn/"+ icons[0] + "@2x.png";
+  var imgalt = descriptions[0];
+
+  document.getElementById("1").textContent = temps[0];
+  document.getElementById("icon1").setAttribute("src",imgsource);
+  document.getElementById("icon1").setAttribute("alt",imgalt);
+
+  var dateObj = new Date(results[0]); 
+  var day1_content = dateObj.toLocaleString("default", { weekday: "long" });
+  const dayname1 = document.getElementById("dayName1");
+  dayname1.textContent = day1_content;
+
+  //SECOND DAY//
+  var imgsource =  "http://openweathermap.org/img/wn/"+ icons[1] + "@2x.png";
+  var imgalt = descriptions[1];
+
+  document.getElementById("2").textContent = temps[1];
+  document.getElementById("icon2").setAttribute("src",imgsource);
+  document.getElementById("icon2").setAttribute("alt",imgalt);
+
+  var dateObj = new Date(results[1]); 
+  var day2_content = dateObj.toLocaleString("default", { weekday: "long" });
+  const dayname2 = document.getElementById("dayName2");
+  dayname2.textContent = day2_content;
+
+  //THIRD DAY//
+  var imgsource =  "http://openweathermap.org/img/wn/"+ icons[2] + "@2x.png";
+  var imgalt = descriptions[2];
+
+  document.getElementById("3").textContent = temps[2];
+  document.getElementById("icon3").setAttribute("src",imgsource);
+  document.getElementById("icon3").setAttribute("alt",imgalt);
+  
+  var dateObj = new Date(results[2]); 
+  var day3_content = dateObj.toLocaleString("default", { weekday: "long" });
+  const dayname3 = document.getElementById("dayName3");
+  dayname3.textContent = day3_content;
+
+  //FOURTH DAY//
+  var imgsource =  "http://openweathermap.org/img/wn/"+ icons[3] + "@2x.png";
+  var imgalt = descriptions[3];
+
+  document.getElementById("4").textContent = temps[3];
+  document.getElementById("icon4").setAttribute("src",imgsource);
+  document.getElementById("icon4").setAttribute("alt",imgalt);
+
+  var dateObj = new Date(results[3]); 
+  var day4_content = dateObj.toLocaleString("default", { weekday: "long" });
+  const dayname4 = document.getElementById("dayName4");
+  dayname4.textContent = day4_content;
+
+  //FIFTH DAY//
+  var imgsource =  "http://openweathermap.org/img/wn/"+ icons[4] + "@2x.png";
+  var imgalt = descriptions[4];
+
+  document.getElementById("5").textContent = temps[4];
+  document.getElementById("icon5").setAttribute("src",imgsource);
+  document.getElementById("icon5").setAttribute("alt",imgalt);
+
+  var dateObj = new Date(results[4]); 
+  var day5_content = dateObj.toLocaleString("default", { weekday: "long" });
+  const dayname5 = document.getElementById("dayName5");
+  dayname5.textContent = day5_content;
+
+});
 
       
 
